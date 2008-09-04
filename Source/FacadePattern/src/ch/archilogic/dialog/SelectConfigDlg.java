13fe494c8656bcfaaf445ae423c03e1a7df1eb4c
Yannick Suter <yannick.suter@gmail.com>
2008-09-04 23:11:11 +0000

    enter side len (added to dialog)
    git-svn-id: https://svn.assembla.com/svn/archilogic@125 e0e46c49-be69-4f5a-ad62-21024a331aea

--- Source/FacadePattern/src/ch/archilogic/dialog/SelectConfigDlg.java
+++ Source/FacadePattern/src/ch/archilogic/dialog/SelectConfigDlg.java
@@ -8,7 +8,10 @@ import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.LayoutStyle;
 import javax.swing.SwingConstants;
 
 import ch.archilogic.solver.config.Config;
@@ -20,6 +23,8 @@ public class SelectConfigDlg extends JDialog implements ActionListener {
 	private static final long serialVersionUID = -3887441711904582173L;
 	
 	private JComboBox box = null;
+	private JFormattedTextField lenTextField = new JFormattedTextField(new Float(1.2));
+	private JLabel lenLabel = new JLabel("Side len [0.4 - 4]:");
 	private JTextArea desc = new JTextArea("Config...");
 	private JButton solveButton = new JButton("Solve");
 	private JButton cancelButton = new JButton("Cancel");
@@ -34,6 +39,9 @@ public class SelectConfigDlg extends JDialog implements ActionListener {
         cancelButton.addActionListener(this);
         box.addActionListener(this);
 
+        lenLabel.setVerticalTextPosition(SwingConstants.BOTTOM);
+        lenLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+        
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setAutoCreateGaps(true);
@@ -42,6 +50,9 @@ public class SelectConfigDlg extends JDialog implements ActionListener {
         layout.setHorizontalGroup(layout.createSequentialGroup()
             .addGroup(layout.createParallelGroup(LEADING)
                 .addComponent(box)
+                .addGroup(GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                		.addComponent(lenLabel)
+                		.addComponent(lenTextField))
                 .addComponent(desc))
             .addGroup(layout.createParallelGroup(LEADING)
                 .addComponent(solveButton)
@@ -55,17 +66,21 @@ public class SelectConfigDlg extends JDialog implements ActionListener {
                 .addComponent(box)
                 .addComponent(solveButton))
             .addGroup(layout.createParallelGroup(LEADING)
+    				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+    					    .addComponent(lenLabel, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+    					    .addComponent(lenTextField, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))          		
+                    .addComponent(cancelButton))
+            .addGroup(layout.createParallelGroup(LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addGroup(layout.createParallelGroup(BASELINE)
-                        .addComponent(desc)))
-                .addComponent(cancelButton))
+                        .addComponent(desc))))
         );
         
         this.box.setSelectedItem(ConfigType.MODEL_DACH_F3);        
 
         setTitle("Select a configuration..");
         setResizable(false);
-        setPreferredSize(new Dimension(350, 150));
+        setPreferredSize(new Dimension(350, 165));
         
         pack();
 
@@ -93,16 +108,17 @@ public class SelectConfigDlg extends JDialog implements ActionListener {
 
     private void setDescText(Config conf) {
     	desc.setBackground(this.getBackground());
-		desc.setText(String.format("Description:\n- object: %s\n"+
-				"- model: %s\n" +
-				"- edge len: %f"
-				, conf.getRefObjPath().replace("file:", ""), conf.getUseThinkModel().getDesc(), conf.getUseEdgeLen()));
+		desc.setText(String.format("Description:\n- object: %s\n- model: %s\n" , conf.getRefObjPath().replace("file:", ""), conf.getUseThinkModel().getDesc()));
     }
     
     public boolean isCanceled() {
 		return canceled;
 	}
 	public Config getConfig() {
+		Float value = (Float)lenTextField.getValue();
+		if (value > 0 && value < 10) {
+			config.setUseEdgeLen(Double.valueOf(value.toString()));
+		}
 		return config;
 	}
 	
